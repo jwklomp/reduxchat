63dd680540528ff627ca4dcad879d86f3e5da637
jwklomp <janwillem.klomp@gmail.com>
2019-01-15 13:52:13 +0100

    first reducers

---
+++ app/store/reducer.js
@@ -0,0 +1,10 @@
+import {
+    combineReducers
+} from 'redux'
+import session from './session/reducer';
+import chat from './chat/reducer'
+
+export default combineReducers({
+    session,
+    chat
+})
